55d8e4f6221077ae520c17ebb1a1191bd4099d94
Sameer Giri <sameergiri1997@gmail.com>
2023-08-22 21:45:17 +0530

    Add Graph and Create Graph functionality

---
+++ Graphs/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Graphs
+{
+	public class Node<T>
+	{
+		private T _data;
+		private List<T> _neighbours;
+		public Node(T data)
+		{
+			_data = data;
+			_neighbours = new();
+		}
+		public T Data { get => _data; }
+		public List<T> Neighbours { get => _neighbours; }
+
+	}
+}
+
